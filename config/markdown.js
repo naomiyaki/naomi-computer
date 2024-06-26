8bf92089690edf784b481a53b81148ffdab07e42
naomiyaki <hello@pumpkin.ink>
2024-06-25 18:32:47 -0700

    [#6] Gracefully prevents build failure if an image isn't found

--- config/markdown.js
+++ config/markdown.js
@@ -37,18 +37,29 @@ markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
     outputDir: `./_site${env.page.url}`
   };
 
-  Image(imgSrc, imgOpts);
-  const metadata = Image.statsSync(imgSrc, imgOpts);
-  const generated = Image.generateHTML(metadata, {
-    sizes: '690w, 1380w',
-    ...htmlOpts
-  });
+  // Run generator in a try block to catch any missing images/errors
+  try {
+    Image(imgSrc, imgOpts);
+    const metadata = Image.statsSync(imgSrc, imgOpts);
+    const generated = Image.generateHTML(metadata, {
+      sizes: '690w, 1380w',
+      ...htmlOpts
+    });
 
-  if (imgTitle) {
-    return figure(generated, imgTitle);
+    if (imgTitle) {
+      return figure(generated, imgTitle);
+    }
+
+    return generated;
+  } catch (error) {
+    // If an image can't be found, the markdown doesn't
+    // render anything and the build continues with a warning
+    console.log(
+      `WARNING: Error processing image ${imgSrc} in markdown: \n`,
+      error
+    );
+    return '';
   }
-
-  return generated;
 };
 
 module.exports = markdown;
