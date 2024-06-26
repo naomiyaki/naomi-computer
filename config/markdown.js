f0dbab9ef74cc8af8084ad7b9eecdbc7f3df5cb3
naomiyaki <hello@pumpkin.ink>
2024-06-25 19:17:00 -0700

    [#6] Add support for external images

--- config/markdown.js
+++ config/markdown.js
@@ -1,5 +1,14 @@
 // Generate responsive images
 // Based on https://tomichen.com/blog/posts/20220416-responsive-images-in-markdown-with-eleventy-image/
+
+// Title Tag Custom Handling
+// ----------
+// @skip[<NUMBER><UNIT>, <NUMBER><UNIT>] skips image/responsive size
+// generation altogether. Important for declaring sizes on an image from another site,
+// or if only one size is desired for some reason.
+// Example:
+// "@skip[100px,200px] Title which also becomes a figure caption"
+
 const markdownOptions = {
   html: true, // Allows HTML
   breaks: true // Converts \n to linebreaks
@@ -9,13 +18,16 @@ const markdown = require('markdown-it')(markdownOptions);
 const Image = require('@11ty/eleventy-img');
 
 markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
+  // Custom HTML for images with a title tag to be used as a caption
   function figure(html, caption) {
     return `<figure>${html}<figcaption>${caption}</figcaption></figure>`;
   }
 
   const token = tokens[idx];
   // Image src is from the markdown project directory
-  let imgSrc = '.' + env.page.url + token.attrGet('src');
+  const imgSrc = token.attrGet('src');
+
+  let imgPath = '.' + env.page.url + token.attrGet('src');
   const imgAlt = token.content;
   const imgTitle = token.attrGet('title');
 
@@ -27,8 +39,36 @@ markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
     decoding: 'async'
   };
 
+  // Parse title text for any custom handling
+  // Skip generating if using custom title code, or if the image is external
+  const titleRegex =
+    /^(?<skip>@skip(?:\[(?<width>\d+[a-z]*),\s*(?<height>\d+[a-z]*)]))\s*(?<titlecap>.*)/;
+  const match = (imgTitle || '').match(titleRegex);
+  const parsed = match ? match.groups : {};
+
+  // If an image is from an external source, skip responsive image generation
+  if (imgSrc.startsWith('http')) {
+    // Get the filetype from the end of the source, to use with metadata
+    // https://stackoverflow.com/questions/680929/how-to-extract-extension-from-filename-string-in-javascript
+    const fileTypeRegex = /(?:\.([^.]+))?$/;
+    const fileType = fileTypeRegex.exec(imgSrc)[1];
+
+    // Adjust HTML options for external image
+    const adjHtmlOpts = { ...htmlOpts };
+    // Apply custom class based on image type
+    adjHtmlOpts.class = 'content-image external-image';
+
+    // Generate metadata based on filetype
+    const metadata = {};
+    metadata[fileType] = [{ url: imgSrc }];
+
+    const generated = Image.generateHTML(metadata, adjHtmlOpts);
+    return generated;
+  }
+
   // Add default image widths
   const imageWidths = [690, 1380, 2070];
+  const imageSizes = '690w, 1380w';
 
   const imgOpts = {
     widths: imageWidths,
@@ -39,10 +79,10 @@ markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
 
   // Run generator in a try block to catch any missing images/errors
   try {
-    Image(imgSrc, imgOpts);
-    const metadata = Image.statsSync(imgSrc, imgOpts);
+    Image(imgPath, imgOpts);
+    const metadata = Image.statsSync(imgPath, imgOpts);
     const generated = Image.generateHTML(metadata, {
-      sizes: '690w, 1380w',
+      sizes: imageSizes,
       ...htmlOpts
     });
 
--- projects/kanji-de-manga/kanji-de-manga.md
+++ projects/kanji-de-manga/kanji-de-manga.md
@@ -38,6 +38,8 @@ Kanji de Manga does exactly this: teach a new character on each page with a purp
 
 ![This is an external image](https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/855.png)
 
+![This is an image where we skip the image generation intentionally](image-test_default@2x.jpg '@skip[3000px, 10vw] Some other title')
+
 My first work on the Kanji de Manga series, as an intern, was on volume 4 as a translation checker. When I came back to work at Japanime in 2007, I became the layout/graphics person and translator on the series. For volumes 1-4, the books were made in Adobe Illustrator, with separate vector files for every single page. One of my first jobs was to port the layout from Adobe Illustrator to Adobe's book layout program InDesign, which made editing and updating the book drastically easier. The new layout also helped us export the book for Kindle and other e-readers, which were just starting to come out at the time.
 
 Each character page has a structured template with example words that use the character, a stroke-order diagram (how to write the character with a pen or brush), and the comic with its translation.
