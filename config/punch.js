f89a5d90109bb6744c518dea7ee7cd61cb1376f1
naomiyaki <hello@pumpkin.ink>
2024-07-16 13:08:18 -0700

    [#15] Update punch container with custom classes

--- config/punch.js
+++ config/punch.js
@@ -7,13 +7,16 @@ module.exports = {
     // allows passing the markdown-it library in-case additional
     // markdown rednering is required
     return function (tokens, idx) {
-      // Match containing the spoiler content
-      // Note that the match should be the same as above
-      const m = tokens[idx].info.trim().match(/^punch\s+(.*)$/);
+      // User can pass a custom class in addition to "punch"
+      // by using the same escape notation as images "@class[classnames]"
+      const classRegex = /(?:@class\[([a-z0-9-\s]*)\])/;
+      const classMatch = tokens[idx].info.trim().match(classRegex);
+      const customClasses =
+        classMatch && classMatch.length >= 2 ? classMatch[1] : '';
 
       if (tokens[idx].nesting === 1) {
         // Return opening tag
-        return '<div class="punch">';
+        return `<div class="punch${customClasses ? ' ' + customClasses : ''}">`;
       } else {
         // Return ending tab
         return '</div>';
